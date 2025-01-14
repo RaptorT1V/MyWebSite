5ecfd2db6c86e498ddda58db841009f5037379e9
raptor <dilan2911@list.ru>
2025-01-14 19:56:29 +0500

    1) Исправил footer ~(padding: 10px)~ в main.css 2) Доделал programmer_resume.html

--- GeniusWebSite/settings.py
+++ GeniusWebSite/settings.py
@@ -130,8 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
 MEDIA_URL = '/media/'
--- programmer_resume/views.py
+++ programmer_resume/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import ProgrammerResume
 
+
 def index(request):
     resume = ProgrammerResume.objects.first()
     return render(request, 'programmer_resume/index.html', {'resume': resume})
